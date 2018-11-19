507e1fcf561e8642e2a5f4e471407679868953a4
James Hunt <jameshunt@Jamess-MBP.nc.rr.com>
2018-11-19 18:47:26 -0500

    back to the start

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -11,10 +11,123 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var weatherLabel: UILabel!
     
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+    let container = UIView()
+    let redSquare = UIView()
+    let blueSquare = UIView()
+    
+    
+    var labeld = UILabel()
+    var labelSum = UILabel()
+    var labelHigh = UILabel()
+    var labelLow = UILabel()
+    
+    ////
+    
+    //
+    
+    
+    
+    var refreshButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
+        // label for CH
+        let labelch = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 150))
+        labelch.numberOfLines = 5
+        labelch.text = "Chapel Hill Weather"
+        labelch.center = CGPoint(x: 130, y: 118)
+        labelch.textAlignment = .justified
+        labelch.textColor = UIColor.black
+        labelch.font = UIFont(name: "Dosis-Bold", size: 35)
+        self.view.addSubview(labelch)
+        
+        //label for time, windspeed, and humidity
+        
+        label.numberOfLines = 112
+        label.center = CGPoint(x: 180, y: 280)
+        label.textAlignment = .natural
+        label.textColor = UIColor.black
+        label.font = UIFont(name: "Dosis-Light", size: 26)
+        self.view.addSubview(label)
+        
         getLocation()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        
+        
+        self.container.frame = CGRect(x: 60, y: 60, width: 200, height: 200)
+        self.view.addSubview(container)
+        
+        
+        self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        
+        
+        let weatherIcon = UIImageView()
+        weatherIcon.image = UIImage(named: "icon_weather.png")
+        weatherIcon.frame = CGRect(x: 180, y: 160, width: 100, height: 100)
+        self.redSquare.addSubview(weatherIcon)
+        
+        
+        
+        
+        self.container.addSubview(self.redSquare)
+        UIView.animate(withDuration: 1.5, animations: {
+            self.redSquare.frame.origin.y -= 160
+        }, completion: nil)
+        
+        
+        
+        
+        
+        // label for low temp
+        labelLow = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        labelLow.numberOfLines = 112
+        labelLow.center = CGPoint(x: 180, y: 450)
+        labelLow.textAlignment = .center
+        labelLow.textColor = UIColor.blue
+        labelLow.font = UIFont(name: "Dosis-Light", size: 30)
+        self.view.addSubview(labelLow)
+        
+        
+        //label for current temp
+        labeld = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        labeld.numberOfLines = 112
+        labeld.center = CGPoint(x: 180, y: 480)
+        labeld.textAlignment = .center
+        labeld.textColor = UIColor.black
+        labeld.font = UIFont(name: "Dosis-Light", size: 30)
+        self.view.addSubview(labeld)
+        
+        // label for max temp
+        labelHigh = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        labelHigh.numberOfLines = 112
+        labelHigh.center = CGPoint(x: 180, y: 510)
+        labelHigh.textAlignment = .center
+        labelHigh.textColor = UIColor.red
+        labelHigh.font = UIFont(name: "Dosis-Light", size: 30)
+        self.view.addSubview(labelHigh)
+        
+        
+        
+        // label for weather summary this week
+        
+        labelSum = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        labelSum.numberOfLines = 112
+        labelSum.center = CGPoint(x: 180, y: 650)
+        labelSum.textAlignment = .center
+        labelSum.textColor = UIColor.black
+        labelSum.font = UIFont(name: "Dosis-Light", size: 30)
+        self.view.addSubview(labelSum)
+        
+        // buttton
+        
+        let button = UIButton(frame: CGRect(x: 150, y: 740, width: 80, height: 30))
+        button.backgroundColor = .black
+        button.setTitle("refresh", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(buttonAction), for: UIControl.Event.touchUpInside)
+        
+        self.view.addSubview(button)
     }
     func getLocation() {
         
@@ -86,13 +199,20 @@ class ViewController: UIViewController {
                 let dateFormatter = DateFormatter()
                 dateFormatter.timeZone = TimeZone(abbreviation: "EDT") //Set timezone that you want
                 dateFormatter.locale = NSLocale.current
-                dateFormatter.dateFormat = "MM-dd h:mm" //Specify your format that you want
+                dateFormatter.dateFormat = "MM-dd h:mm:ss" //Specify your format that you want
                 let strDate = dateFormatter.string(from: date)
                 
                 
                 
                 
-                self.weatherLabel.text = "time: \(strDate)\nwindspeed: \(windSpeed) MPH\nhumidity: \(Int(humidity))%\nnearest storm: \(nearestStormDistance) miles\ncloud cover: \(cloudCover)%\nprecipitation probability: \(precipProbability)%"
+                self.label.text = "time: \(strDate)\nwindspeed: \(windSpeed) MPH\nhumidity: \(Int(humidity*100))%\nnearest storm: \(nearestStormDistance) miles\ncloud cover: \(cloudCover)%\nprecipitation probability: \(precipProbability)%"
+                
+                
+                self.labelHigh.text = "\(tempHigh) ˚F"
+                self.labeld.text = "\(temp) ˚F"
+                self.labelLow.text = "\(tempLow) ˚F"
+                self.labelSum.text = summary
+
                 
             }
         }
@@ -100,7 +220,9 @@ class ViewController: UIViewController {
         
     }
     
-        
+    @objc func buttonAction(sender: UIButton!) {
+        getLocation()
+    }
     
     
 
